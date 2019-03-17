73a159072709a464841bf653b7c4ea71c6bf614f
Nimmi Ghetia <ngghetia@gmail.com>
2019-03-17 05:51:53 +0530

    not working

--- lab7/MsAccess.py
+++ lab7/MsAccess.py
@@ -1,1 +1,9 @@
+import pyodbc
+import platform 
 
+platform.architecture()
+#conn=pyodbc.connect(r'Driver={Microsoft Access Driver(*.accdb)};DBQ=/home/FDUSER/Data-Warehouse/lab7/Books2010.accdb;')
+#cursor=conn.cursor()
+#cursor.execute('Select * from books')
+#for row in cursor.fetchall():
+#    print(row)
